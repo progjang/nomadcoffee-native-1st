29697c61d9f62881d976ed0eb10c8689ed6a4f4e
PJ <wooritobi@gmail.com>
2021-06-14 23:35:51 +0900

    code for preloading fonts and images  & Preload Ionicons

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import {Asset} from 'expo-asset';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish  = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font)=> Font.loadAsync(font));
+    const imagesToLoad = [require("./assets/insta-logo.png"),
+  "https://www.citypng.com/public/uploads/preview/-11590321548vfiwckfjs3.png"];
+  const imagePromises = imagesToLoad.map(image=>
+    Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
+  };
+  if(loading){
+    return (
+      <AppLoading 
+      startAsync={preload} 
+      onError={console.warn} 
+      onFinish={onFinish} />
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world````!</Text>
       <StatusBar style="auto" />
     </View>
   );
